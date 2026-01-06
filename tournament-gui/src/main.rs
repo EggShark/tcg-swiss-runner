7b72b8fe3548a3b9bc77e8b6e3faebd36c93177b
EggShark <eggshark@eggshark.dev>
2026-01-06 15:10:05 -0700

    bare min iced

--- tournament-gui/src/main.rs
+++ tournament-gui/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello World!");
+    let _ = iced::run(TournamentApp::update, TournamentApp::view);
+}
+
+type Message = ();
+
+#[derive(Default)]
+struct TournamentApp;
+
+impl TournamentApp {
+    fn update(&mut self, _message: Message) {
+
+    }
+
+    fn view(&self) -> iced::Element<Message> {
+        "hello world!".into()
+    }
 }
